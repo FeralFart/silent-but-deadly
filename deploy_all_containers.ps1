9f9056330e179c5308096c2da97d6e1098a58ba7
FeralFart <kendik@gmail.com>
2026-03-22 13:56:22 +0000

    Add deploy_all_containers.ps1

---
+++ deploy_all_containers.ps1
@@ -0,0 +1,25 @@
+$root = "E:\Docker" #Amend this to match deployment location for docker containers
+
+$yamls = Get-ChildItem $root -Recurse -Depth 2 -File -Include *.yml, *.yaml
+
+foreach ($file in $yamls) {
+
+    Write-Host "--------------------------------"
+    Write-Host "Deploying $($file.FullName)"
+    Write-Host "--------------------------------"
+
+    $dir = $file.DirectoryName
+    $name = $file.Name
+
+    Push-Location $dir
+
+    try {
+        docker compose -f $name up -d
+        Write-Host "SUCCESS: $name deployed" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "FAILED: $name" -ForegroundColor Red
+    }
+
+    Pop-Location
+}
